4d8890334d898e42fc557d4907e8828dba216807
Cody Morley <codymorleyinc@gmail.com>
2020-05-13 14:03:03 -0700

    app canvas is ready to draw.

---
+++ Logo/Logo/LogoView.swift
@@ -0,0 +1,23 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Cody Morley on 5/13/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class LogoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Logo/Logo/LogoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogoViewController.swift
+//  Logo
+//
+//  Created by Cody Morley on 5/13/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
